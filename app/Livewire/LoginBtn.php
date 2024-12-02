33a1dd1650775a8a1508c9cea58e5af867484fb2
Nurlan270 <nurlanismzade@gmail.com>
2024-12-02 21:39:03 +0400

    Login btn added

---
+++ app/Livewire/LoginBtn.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LoginBtn extends Component
+{
+
+    public function showModal()
+    {
+        $this->js("document.querySelector('#')");
+
+    }
+}
