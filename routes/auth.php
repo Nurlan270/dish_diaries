2c05992f47a68d191572018804d24e4e1e048028
Nurlan270 <nurlanismzade@gmail.com>
2024-12-02 21:40:06 +0400

    auth routes added

---
+++ routes/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\OAuth\GoogleController;
+
+Route::controller(GoogleController::class)->group(function () {
+    Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
+    Route::get('auth/google/callback', 'handleGoogleCallback');
+});
