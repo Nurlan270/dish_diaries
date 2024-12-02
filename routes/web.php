46c36eb59fdb2d02bb3efca50ade3f236cf981da
Nurlan270 <nurlanismzade@gmail.com>
2024-12-02 21:41:09 +0400

    Terms & Policy routes added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require 'auth.php';
+
+Route::view('/', 'pages.main')->name('main');
+
+Route::view('/policy', 'pages.agreements.policy')->name('agreements.policy');
+Route::view('/terms', 'pages.agreements.terms')->name('agreements.terms');
+
+
