560ca45acf91807124529dbc3c70bf45d89304e9
Nurlan270 <nurlanismzade@gmail.com>
2024-12-04 19:33:53 +0400

    few styles added/removed

--- resources/views/livewire/theme-toggler.blade.php
+++ resources/views/livewire/theme-toggler.blade.php
@@ -1,4 +1,4 @@
 <button wire:click="changeTheme"
-        class="cursor-pointer transition-colors mx-1 p-2 w-10 rounded-xl h-auto dark:hover:bg-gray-700 hover:bg-gray-200 text-gray-500 dark:text-gray-400">
+        class="cursor-pointer transition-colors ms-2 p-2 w-10 rounded-xl h-auto dark:hover:bg-gray-700 hover:bg-gray-200 text-gray-500 dark:text-gray-400">
     @svg('mdi-theme-light-dark')
 </button>
