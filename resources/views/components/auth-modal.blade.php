5f62fa0d7b7401e3267ec7e3f8568304fe12bcff
Nurlan270 <nurlanismzade@gmail.com>
2024-12-04 19:35:10 +0400

    Auth modal (register/login) added, header updated

---
+++ resources/views/components/auth-modal.blade.php
@@ -0,0 +1,176 @@
+@guest
+    <div>
+        <button
+            data-modal-target="authentication-modal"
+            data-modal-toggle="authentication-modal"
+            class="text-white bg-blue-700 hover:bg-blue-800 transition-colors font-medium rounded-lg text-sm ms-2 px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
+            type="button">
+            Get started
+        </button>
+
+        {{-- Auth modal --}}
+        <div id="authentication-modal" tabindex="-1" aria-hidden="true"
+             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-lg max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    {{-- Modal header --}}
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white flex space-x-2 items-center">
+                            <x-phosphor-keyhole-duotone class="w-7 h-auto"/>
+                            <span>Sign in to our platform</span>
+                        </h3>
+                        <button type="button"
+                                class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="authentication-modal">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                 viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                      stroke-width="2"
+                                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+
+                    {{-- Modal body --}}
+                    <div class="p-4 md:p-5 flex flex-wrap justify-center gap-5">
+                        <button onclick="window.location.href='{{ route('auth.google') }}'"
+                                class="dark:bg-black dark:text-white dark:border-none bg-white hover:bg-gray-100 transition-colors text-gray-600 text-sm border-2 px-5 py-3 w-full sm:w-fit rounded-2xl font-bold cursor-pointer flex items-center justify-center gap-x-3">
+                            <x-icons.google/>
+                            Sign in with Google
+                        </button>
+
+                        <button onclick="window.location.href='{{ route('auth.google') }}'"
+                                class="dark:bg-gray-500 dark:text-black dark:border-none bg-white hover:bg-gray-100 transition-colors text-black text-sm border-2 px-5 py-3 w-full sm:w-fit rounded-2xl font-bold cursor-pointer flex items-center justify-center gap-x-3">
+                            <x-icons.github/>
+                            Sign in with GitHub
+                        </button>
+
+                        <div class="inline-flex items-center justify-center w-full">
+                            <hr class="w-64 h-px my-4 bg-gray-400 border-0 dark:bg-gray-400">
+                            <span
+                                class="absolute px-3 font-medium text-sm text-gray-900 -translate-x-1/2 bg-white dark:bg-gray-700 left-1/2 dark:text-white">OR</span>
+                        </div>
+
+                        <button
+                            data-modal-hide="authentication-modal"
+                            data-modal-target="login-form-modal"
+                            data-modal-toggle="login-form-modal"
+                            class="dark:bg-blue-600 dark:text-white dark:border-none hover:bg-gray-100 transition-colors text-blue-600 text-sm border-2 px-5 py-3 w-full sm:w-fit rounded-2xl font-bold cursor-pointer flex items-center justify-center gap-x-3">
+                            <x-ionicon-mail class="w-6 h-auto"/>
+                            Sign in with Email
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{--    Login Modal    --}}
+        <div id="login-form-modal" tabindex="-1" aria-hidden="true"
+             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-lg max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    {{-- Modal header --}}
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white flex space-x-2 items-center">
+                            <x-phosphor-keyhole-duotone class="w-7 h-auto"/>
+                            <span>Sign in</span>
+                        </h3>
+                        <div>
+                            <button
+                                data-modal-hide="login-form-modal"
+                                data-modal-target="register-form-modal"
+                                data-modal-toggle="register-form-modal"
+                                class="me-3 underline inline-flex text-sm font-semibold gap-x-1 dark:text-white text-black">
+                                SIGN UP
+                                <x-untitledui-link-external-01 class="w-3.5 h-auto"/>
+                            </button>
+                            <button type="button"
+                                    class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    data-modal-hide="login-form-modal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2"
+                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                    </div>
+
+                    {{-- Modal body --}}
+                    <div class="p-4 md:p-5">
+                        <x-input-field type="email" name="email" label="Email"/>
+
+                        <x-input-field type="password" name="password" label="Password"/>
+
+                        <div class="flex items-center justify-between flex-wrap">
+                            <button class="dark:text-white text-blue-600 text-sm underline">
+                                Forgot password?
+                            </button>
+
+                            <button
+                                class="text-white bg-blue-700 hover:bg-blue-800 transition-colors font-medium rounded-lg text-sm px-6 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700">
+                                Sign in
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{--    Register Modal    --}}
+        <div id="register-form-modal" tabindex="-1" aria-hidden="true"
+             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-lg max-h-full">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    {{-- Modal header --}}
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white flex space-x-2 items-center">
+                            <x-phosphor-keyhole-duotone class="w-7 h-auto"/>
+                            <span>Sign up</span>
+                        </h3>
+                        <div>
+                            <button
+                                data-modal-hide="register-form-modal"
+                                data-modal-target="login-form-modal"
+                                data-modal-toggle="login-form-modal"
+                                class="me-3 underline inline-flex text-sm font-semibold gap-x-1 dark:text-white text-black">
+                                SIGN IN
+                                <x-untitledui-link-external-01 class="w-3.5 h-auto"/>
+                            </button>
+                            <button type="button"
+                                    class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    data-modal-hide="register-form-modal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2"
+                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                    </div>
+
+                    {{-- Modal body --}}
+                    <div class="p-4 md:p-5 flex flex-col justify-center">
+                        <x-input-field type="text" name="username" label="Username"/>
+
+                        <x-input-field type="email" name="email" label="Email"/>
+
+                        <x-input-field type="password" name="password" label="Password"/>
+
+                        <x-input-field type="password" name="password_confirmation" label="Password confirmation"/>
+
+                        <button
+                            class="text-white bg-blue-700 hover:bg-blue-800 transition-colors font-medium rounded-lg text-sm py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700">
+                            Sign up
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endguest
--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -10,7 +10,7 @@
             <div class="flex md:order-2">
                 <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
                         aria-expanded="false"
-                        class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+                        class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5 me-1">
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                          viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -29,14 +29,14 @@
                         <span class="sr-only">Search icon</span>
                     </div>
                     <input type="text" id="search-navbar"
-                           class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                            placeholder="Search...">
                 </div>
                 <livewire:theme-toggler/>
-                <livewire:login-btn />
+                <x-auth-modal />
 
                 <button data-collapse-toggle="navbar-search" type="button"
-                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        class="inline-flex items-center ms-2 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700"
                         aria-controls="navbar-search" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -46,7 +46,6 @@
                     </svg>
                 </button>
             </div>
-
 
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
                 <div class="relative mt-3 md:hidden">
