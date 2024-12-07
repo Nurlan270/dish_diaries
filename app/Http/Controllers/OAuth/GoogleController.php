efe5158d87b4b6f890408b20c7737ebb6da0f824
Nurlan270 <nurlanismzade@gmail.com>
2024-12-07 19:48:20 +0400

    Error handling added

--- app/Http/Controllers/OAuth/GoogleController.php
+++ app/Http/Controllers/OAuth/GoogleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\OAuth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -19,19 +20,25 @@ class GoogleController extends Controller
 
     public function handleGoogleCallback()
     {
-        $googleUser = Socialite::driver('google')->user();
-        $user = User::where('google_id', $googleUser->id)->first();
+        try {
+            $googleUser = Socialite::driver('google')->user();
+            $user = User::where('google_id', $googleUser->id)->first();
 
-        if ($user) {
-            Auth::login($user);
-        } else {
-            $newUser = User::updateOrCreate(['email' => $googleUser->email], [
-                'google_id' => $googleUser->id,
-                'name'      => $googleUser->name,
-                'password'  => bcrypt(Str::random(32)),
-            ]);
+            if ($user) {
+                Auth::login($user);
+            } else {
+                $newUser = User::updateOrCreate(['email' => $googleUser->email], [
+                    'google_id' => $googleUser->id,
+                    'name'      => $googleUser->name,
+                    'password'  => bcrypt(Str::random(32)),
+                ]);
 
-            Auth::login($newUser);
+                Auth::login($newUser);
+
+                notyf()->success('Registered successfully');
+            }
+        } catch (\Throwable $e) {
+            notyf()->error('Error occurred while registering, please try again');
         }
 
         return redirect()->route('main');
