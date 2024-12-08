e05ea4c538d19610bf39511399510cb765e582c5
Nurlan270 <nurlanismzade@gmail.com>
2024-12-08 13:23:15 +0400

    Notification markdown updated

--- resources/views/notifications/send-password.blade.php
+++ resources/views/notifications/send-password.blade.php
@@ -1,27 +1,22 @@
 <x-mail::message>
     <p><strong>Dear {{ $name }},</strong></p>
 
-    We’re thrilled to welcome you to **Dish Diaries**, your personal space for discovering, sharing, and documenting
-    culinary delights.
+    <p>We’re thrilled to welcome you to **Dish Diaries**, your personal space for discovering, sharing, and documenting
+        culinary delights.</p>
 
-    Your account has been successfully created, and you’re just one step away from exploring our vibrant community of
-    food lovers.
+    <p>Your account has been successfully created, and you’re just one step away from exploring our vibrant community of
+        food lovers.</p>
 
     <x-mail::button :$url color="green">
         Explore more!
     </x-mail::button>
 
-    <hr>
 
-    <x-mail::panel>
-        - Email:    {{ $email }}
-        - Password: {{ $password }}
-    </x-mail::panel>
+    <p>- Email:    {{ $email }}</p>
+    <p>- Password: {{ $password }}</p>
 
     <p><strong>Note:</strong> For your security, we recommend changing your password after logging in for the first
         time.</p>
-
-    <hr>
 
     <strong>Bon appétit,</strong><br>
     <i>The {{ config('app.name') }} Team</i>
