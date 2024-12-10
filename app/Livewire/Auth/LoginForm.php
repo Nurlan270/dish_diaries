4c43c63d700fd92062e91fef3692c58805afd90e
Nurlan270 <nurlanismzade@gmail.com>
2024-12-10 19:52:23 +0400

    error messages made more informative

--- app/Livewire/Auth/LoginForm.php
+++ app/Livewire/Auth/LoginForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Auth;
 
 use App\Livewire\Forms\LoginUserForm;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 
 class LoginForm extends Component
@@ -13,9 +14,11 @@ class LoginForm extends Component
     public function loginUser()
     {
         try {
-            if (Auth::attempt($this->form->validate())) {
+            if (Auth::attempt($this->form->validate(), $this->form->only('remember'))) {
                 notyf()->success('Logged in successfully');
             }
+        } catch (ValidationException $e) {
+            notyf()->error('Login failed. Please check the information provided');
         } catch (\Throwable $e) {
             notyf()->error('Error occurred while logging, please try again');
         }
