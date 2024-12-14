77618672b145c8c1b4bad60863b03b22fc97d459
Nurlan270 <nurlanismzade@gmail.com>
2024-12-14 18:58:45 +0400

    submit btn text updated

--- resources/views/livewire/auth/send-reset-link.blade.php
+++ resources/views/livewire/auth/send-reset-link.blade.php
@@ -7,6 +7,6 @@
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 transition-colors font-medium rounded-lg text-sm px-6 py-2 text-center flex items-center justify-center">
         <x-loader />
-        <p wire:loading.remove>Send</p>
+        <p wire:loading.remove>Send reset link</p>
     </button>
 </form>
