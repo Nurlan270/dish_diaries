e727e567c4a90eb6fd5310361093c15545315990
Nurlan270 <nurlanismzade@gmail.com>
2024-12-14 18:39:17 +0400

    password reset route added + OAuth2 routes optimized

--- routes/auth.php
+++ routes/auth.php
@@ -2,19 +2,22 @@
 
 use App\Http\Controllers\OAuth\GitHubController;
 use App\Http\Controllers\OAuth\GoogleController;
+use App\Livewire\Auth\ResetPassword;
 
-/*
- * OAuth
- */
+Route::middleware('guest')->group(function () {
+    /*     OAuth2    */
+    //  Google
+    Route::controller(GoogleController::class)->group(function () {
+        Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
+        Route::get('auth/google/callback', 'handleGoogleCallback');
+    });
 
-//  Google
-Route::controller(GoogleController::class)->group(function () {
-    Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
-    Route::get('auth/google/callback', 'handleGoogleCallback');
+    //  GitHub
+    Route::controller(GitHubController::class)->group(function () {
+        Route::get('auth/github', 'redirectToGithub')->name('auth.github');
+        Route::get('auth/github/callback', 'handleGithubCallback');
+    });
 });
 
-//  GitHub
-Route::controller(GitHubController::class)->group(function () {
-    Route::get('auth/github', 'redirectToGithub')->name('auth.github');
-    Route::get('auth/github/callback', 'handleGithubCallback');
-});
+Route::view('/reset-password/{token}', 'pages.main')
+    ->middleware('guest')->name('password.reset');
