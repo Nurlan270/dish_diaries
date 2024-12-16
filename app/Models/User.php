f2a7fa8168d47f856c6543035f6cd0fbbc8a1e73
Nurlan270 <nurlanismzade@gmail.com>
2024-12-16 19:30:48 +0400

    testing avatar logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable implements CanResetPassword, HasLocalePrefere
      * @var array<int, string>
      */
     protected $fillable = [
+        'oauth_id',
         'avatar',
         'username',
         'email',
