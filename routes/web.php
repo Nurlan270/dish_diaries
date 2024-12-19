67bc5e130e0b183a33f0d8805a7284337d7c1e71
Nurlan270 <nurlanismzade@gmail.com>
2024-12-19 19:25:20 +0400

    routes updated to support locales

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 require 'auth.php';
 
-Route::view('/', 'pages.main')->name('main');
+Route::prefix(LaravelLocalization::setLocale())->group(function () {
+    Route::view('/', 'pages.main')->name('main');
 
-Route::view('/policy', 'pages.agreements.policy')->name('agreements.policy');
-Route::view('/terms', 'pages.agreements.terms')->name('agreements.terms');
+    Route::view('/policy', 'pages.agreements.policy')->name('agreements.policy');
+    Route::view('/terms', 'pages.agreements.terms')->name('agreements.terms');
+});
 
 
