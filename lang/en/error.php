f72a31399a4df226f1d8afb163cdb4c0bafb77ff
Nurlan270 <nurlanismzade@gmail.com>
2024-12-26 16:58:20 +0400

    lang added

---
+++ lang/en/error.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    '404' => [
+        'page_title'  => 'Not Found',
+        'title'       => "Look like you're lost",
+        'description' => 'the page you are looking for not available!',
+    ],
+];
