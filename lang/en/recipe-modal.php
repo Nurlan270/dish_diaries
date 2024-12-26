48237f1203b743593e2fad1397a61523c55a2886
Nurlan270 <nurlanismzade@gmail.com>
2024-12-26 17:09:21 +0400

    lang added

---
+++ lang/en/recipe-modal.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'tooltip'      => 'Create new recipe',
+    'title'        => 'Create New Recipe',
+    'field-titles' => [
+        'title'       => 'Title',
+        'description' => 'Description',
+        'ingredients' => 'Ingredients',
+        'images'      => [
+            'title' => "Upload recipe's images",
+            'tips'  => 'PNG, JPG or JPEG (MAX. 10MB).',
+        ],
+    ],
+    'draft'        => 'Save to drafts',
+    'create'       => 'Create recipe',
+    'placeholder'  => [
+        'title'       => 'Cheese Cake',
+        'description' => 'A delicious cheese cake recipe.',
+    ],
+];
---
+++ lang/ru/recipe-modal.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'tooltip'      => 'Создать новый рецепт',
+    'title'        => 'Создать новый рецепт',
+    'field-titles' => [
+        'title'       => 'Заголовок',
+        'description' => 'Описание',
+        'ingredients' => 'Ингредиенты',
+        'images'      => [
+            'title' => "Загрузить изображения рецепта",
+            'tips'  => 'PNG, JPG или JPEG (МАКС. 10МБ).',
+        ],
+    ],
+    'draft'        => 'Сохранить в черновики',
+    'create'       => 'Создать рецепт',
+    'placeholder'  => [
+        'title'       => 'Чизкейк',
+        'description' => 'Вкусный рецепт чизкейка.',
+    ],
+];
