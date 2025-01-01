00516142256a1c5483a30c68e2d7e58abe92d3cc
Nurlan270 <nurlanismzade@gmail.com>
2025-01-01 16:59:15 +0400

    optimized

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,5 +1,4 @@
 <header>
-
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 px-2 sm:p-4">
             <a href="{{ config('app.url') }}" class="flex items-center rtl:space-x-reverse">
@@ -37,7 +36,7 @@
                 </div>
 
                 @guest
-                    <livewire:theme-toggler/>
+                    <livewire:partials.theme-toggler/>
                     <x-locale-switcher/>
                 @endguest
 
@@ -58,7 +57,38 @@
                         <div class="tooltip-arrow" data-popper-arrow></div>
                     </div>
 
-                    <livewire:create-recipe-modal/>
+                    <div>
+                        <div id="create-recipe-modal" tabindex="-1" aria-hidden="true"
+                             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-xl max-h-full">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <!-- Modal header -->
+                                    <div
+                                        class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white flex space-x-2 items-center">
+                                            <x-akar-reciept class="w-7 h-auto"/>
+                                            <span>{{ __('recipe-modal.title') }}</span>
+                                        </h3>
+                                        <button type="button"
+                                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                                data-modal-toggle="create-recipe-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w.org/2000/svg"
+                                                 fill="none"
+                                                 viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                      stroke-linejoin="round" stroke-width="2"
+                                                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                            </svg>
+                                            <span class="sr-only">{{ __('auth.close-modal') }}</span>
+                                        </button>
+                                    </div>
+                                    <!-- Modal body -->
+                                    <livewire:create-recipe-modal/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     {{--        Profile        --}}
                     <button id="dropdownUserAvatarButton" data-dropdown-toggle="dropdownAvatar"
@@ -77,13 +107,11 @@
                             <div class="font-medium text-xs truncate">{{ Auth::user()->email }}</div>
                         </div>
                         <div class="flex p-2 items-center justify-between">
-                            <livewire:theme-toggler
+                            <livewire:partials.theme-toggler
                                 class="text-gray-500 border border-gray-300 rounded-lg hover:bg-gray-200 dark:bg-gray-700 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"/>
 
                             <div class="flex gap-x-1">
-                                <form id="en-switcher" action="{{ route('locale.switch', ['locale' => 'en']) }}"
-                                      method="POST">@csrf</form>
-                                <form id="ru-switcher" action="{{ route('locale.switch', ['locale' => 'ru']) }}"
+                                <form id="{{ getOppositeLocale() }}-switcher" action="{{ route('locale.switch', ['locale' => getOppositeLocale()]) }}"
                                       method="POST">@csrf</form>
                                 <button id="states-button"
                                         class="flex-shrink-0 z-10 inline-flex items-center gap-x-1 py-2 px-2 text-sm font-medium text-center text-gray-500 border border-gray-300 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 {{ markIfLocale('en') }}"
@@ -179,5 +207,4 @@
             </div>
         </div>
     </nav>
-
 </header>
