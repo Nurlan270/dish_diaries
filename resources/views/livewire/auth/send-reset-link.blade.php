08b843c565bd29aae67ad40c47e28453e091801f
Nurlan270 <nurlanismzade@gmail.com>
2025-01-01 17:01:48 +0400

    updated

--- resources/views/livewire/auth/send-reset-link.blade.php
+++ resources/views/livewire/auth/send-reset-link.blade.php
@@ -6,7 +6,7 @@
         wire:loading.class.remove="hover:bg-blue-800 dark:hover:bg-blue-700"
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 transition-colors font-medium rounded-lg text-sm px-6 py-2 text-center flex items-center justify-center">
-        <x-loader />
+        <x-loader wire:loading/>
         <p wire:loading.remove>{{ __('auth.send-reset-link') }}</p>
     </button>
 </form>
