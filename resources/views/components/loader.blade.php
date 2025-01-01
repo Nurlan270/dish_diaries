85ac9d012483a3dea2fb39ad09dd060d7928aeb1
Nurlan270 <nurlanismzade@gmail.com>
2025-01-01 17:00:06 +0400

    updated

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,6 +1,6 @@
-<span wire:loading {{ $attributes }}>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="size-5"
-         style="shape-rendering: auto; display: block; background: transparent;" width="200" height="200"
+<span {{ $attributes }}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="size-5 {{ $class ?? '' }}"
+         style="shape-rendering: auto; display: block; background: transparent;"
          xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle stroke-linecap="round" fill="none"
                                                                stroke-dasharray="50.26548245743669 50.26548245743669"
                                                                stroke="#ffffff" stroke-width="8" r="32" cy="50" cx="50">
